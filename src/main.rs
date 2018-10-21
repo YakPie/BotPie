a6637d8460c1523821a89b79ddb964fc794794ec
Frank Lyder Bredland <fl@nyggen.com>
2018-10-21 17:32:30 +0200

    Work towards #3. Added SQLite

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 extern crate irc;
+extern crate rusqlite;
+extern crate chrono;
 
 use irc::client::prelude::*;
 use std::env;
+use rusqlite::Connection;
+use chrono::prelude::*;
 
 fn main() {
-    // We can also load the Config at runtime via Config::load("path/to/config.toml")
+    // IRC stup
+    // TODO: refactor out into its own module
     let config = Config {
         nickname: Some("yakpie".to_owned()),
         server: Some("irc.chat.twitch.tv".to_owned()),
@@ -17,18 +22,47 @@ fn main() {
     let client = reactor.prepare_client_and_connect(&config).unwrap();
     client.identify().unwrap();
 
-    reactor.register_client_with_handler(client, |client, message| {
+    // SQLite setup
+    let conn = Connection::open_in_memory().unwrap();
+    conn.execute("CREATE TABLE command (
+                  id              INTEGER PRIMARY KEY,
+                  name            TEXT NOT NULL,
+                  text            TEXT NOT NULL
+                  )", &[]).unwrap();  
+
+    reactor.register_client_with_handler(client, move |client, message| {
         print!("{}", message);			
 
 		if let Command::PRIVMSG(channel, message) = message.command {
-			if message.contains("!repo") {
+            if message.contains("!help") {
+                let mut stmt = conn.prepare("SELECT name FROM command").unwrap(); 
+				client.send_privmsg(&channel, "Help commands:").unwrap();
+                let command_iter = stmt.query_map(&[], |row| {
+                    let name : String = row.get(0);
+                    return name;
+                }).unwrap();
+                for command in command_iter {
+				    client.send_privmsg(&channel, command.unwrap()).unwrap();
+                }
+            }
+            else if message.contains("!addcommand") {
+                let utc: DateTime<Utc> = Utc::now();
+                conn.execute(
+                    "INSERT INTO command (name, text) VALUES (?1, ?2)",
+                    &[&"test".to_string(), &"test".to_string()]
+                ).unwrap();
+                print!("Tries to insert data into the database\n");
+            }
+            else if message.contains("!repo") {
 				client.send_privmsg(&channel, "https://github.com/YakPie/BotPie").unwrap();
 			}
-
-			if message.contains("!schedule") {
+            else if message.contains("!schedule") {
                 client.send_privmsg(&channel, "Check out schedule over at https://yakpie.com/").unwrap();
-			}	
-
+			}
+            else if message.contains("!") {
+                // TODO: database lookup for command
+            }
+            
 			if message.contains(client.current_nickname()) {
 				client.send_privmsg(&channel, "beep boop").unwrap();
 			}

94829aba9d95762c5b1f55981ea6702562430e84
Frank Lyder Bredland <fl@nyggen.com>
2018-10-21 16:23:54 +0200

    First version of a functioning Twitch IRC bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate irc;
+
+use irc::client::prelude::*;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // We can also load the Config at runtime via Config::load("path/to/config.toml")
+    let config = Config {
+        nickname: Some("yakpie".to_owned()),
+        server: Some("irc.chat.twitch.tv".to_owned()),
+        channels: Some(vec!["#yakpie".to_owned()]),
+        password: Some(env::var("twitch_oath").unwrap()),
+        ..Config::default()
+    };
+
+    let mut reactor = IrcReactor::new().unwrap();
+    let client = reactor.prepare_client_and_connect(&config).unwrap();
+    client.identify().unwrap();
+
+    reactor.register_client_with_handler(client, |client, message| {
+        print!("{}", message);
+		if let Command::PRIVMSG(channel, message) = message.command {
+			if message.contains(client.current_nickname()) {
+				client.send_privmsg(&channel, "beep boop").unwrap();
+			}
+		}
+        Ok(())
+    });
+
+    reactor.run().unwrap();
 }
